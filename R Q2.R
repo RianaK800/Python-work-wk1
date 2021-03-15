6a3d33f9be2338aea917ef60b6ded403beac1cfa
RianaK800 <77251750+RianaK800@users.noreply.github.com>
2021-03-15 18:33:28 +0000

    Add files via upload

---
+++ R Q2.R
@@ -0,0 +1,10 @@
+Employees = data.frame(Name=c("Sarah S","Adam J","James B", "Ashley M", "Lisa S"),
+                       Gender=c("F","M","M","F","M"),
+                       Age=c(25,26,32,22,23),
+                       Role=c("Manager","Executive","CEO","Assistant","Clerk"),
+                       Length_of_Service=c(8,10,15,2,3)
+                      )
+
+                        
+print("Details of the employees:")
+print(Employees)
