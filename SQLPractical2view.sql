c3232c1cf8b0ab8d0f8664637ad79c668d1b1e18
RianaK800 <77251750+RianaK800@users.noreply.github.com>
2021-03-29 23:27:18 +0100

    Add files via upload

--- SQLPractical2view.sql
+++ SQLPractical2view.sql
@@ -3,3 +3,8 @@ select* from [dbo].[SQLPractical] where embarked ='Southampton'
 select Gender, Age from [dbo].[SQLPractical] where embarked ='Cherbourg'and Age = 22
 
 select * from [dbo].[SQLPractical] order by Age asc
+
+
+SELECT *
+FROM  SQLPractical INNER JOIN
+         Table_2 ON SQLPractical.TitanicID = Table_2.Titanicid
---
+++ Table2_InsertRecords.sql
@@ -0,0 +1,14 @@
+USE [Titanic]
+GO
+
+INSERT INTO [dbo].[Table_2]
+           ([Titanicid],[Survived],[First],[Last])      VALUES      (1,1,'Laina','Heikinen')
+
+INSERT INTO [dbo].[Table_2]
+           ([Titanicid],[Survived],[First],[Last])      VALUES      (2, 0,'John','Cumings')
+INSERT INTO [dbo].[Table_2]
+           ([Titanicid],[Survived],[First],[Last])      VALUES      (4, 1,'Ian','Smith')
+INSERT INTO [dbo].[Table_2]
+           ([Titanicid],[Survived],[First],[Last])      VALUES      (5,0,'LIsa','Wilson')
+INSERT INTO [dbo].[Table_2]
+           ([Titanicid],[Survived],[First],[Last])      VALUES      (6,1,'David','Stokes')
