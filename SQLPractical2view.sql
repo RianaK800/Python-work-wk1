04684abc2513144060f4ae41de5a5280d39b2b1a
RianaK800 <77251750+RianaK800@users.noreply.github.com>
2021-03-29 21:46:04 +0100

    Add files via upload

--- SQLPractical2view.sql
+++ SQLPractical2view.sql
@@ -1,3 +1,5 @@
 select * from [dbo].[SQLPractical]
 select* from [dbo].[SQLPractical] where embarked ='Southampton'
 select Gender, Age from [dbo].[SQLPractical] where embarked ='Cherbourg'and Age = 22
+
+select * from [dbo].[SQLPractical] order by Age asc
